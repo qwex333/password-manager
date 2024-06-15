dbecec0ddfa3b91660e9647fbcafedf120224486
james <qwex3333@gmail.com>
2024-06-16 00:13:52 +0800

    use django User model

--- backend/home/models.py
+++ backend/home/models.py
@@ -6,22 +6,22 @@ from django.contrib.auth.models import User
 
 # eache variable in class obj is a column in DB
 
-class UserAccount(models.Model):
-    username = models.CharField(max_length=30, unique=True)
-    password = models.CharField(max_length=30)
-    reg_time = models.TimeField("time registered", default=datetime.now())
+# class UserAccount(models.Model):
+#     username = models.CharField(max_length=30, unique=True)
+#     password = models.CharField(max_length=30)
+#     reg_time = models.TimeField("time registered", default=datetime.now())
 
-    def __str__(self) -> str:
-        return self.username
+#     def __str__(self) -> str:
+#         return self.username
 
 class PasswordData(models.Model):
-    user = models.ForeignKey(UserAccount, to_field="username", related_name="passworddata", on_delete=models.CASCADE)
-    # user = models.ForeignKey(User, related_name="passworddata", on_delete=models.CASCADE)
+    # user = models.ForeignKey(UserAccount, to_field="username", related_name="passworddata", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name="passworddata", on_delete=models.CASCADE)
 
     subject = models.CharField(max_length=30)
     account = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
-    reg_time = models.TimeField("time registered", default=datetime.now())
+    date_added = models.DateField("date added", default=datetime.today())
 
     def __str__(self) -> str:
         return self.subject + " -- " + self.account
--- backend/home/views.py
+++ backend/home/views.py
@@ -5,23 +5,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from datetime import datetime
-from .models import UserAccount, PasswordData
+from .models import PasswordData
 from django.template import loader
 
 from django.contrib import messages
 from django.shortcuts import redirect
 
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 def index(request):
     # return HttpResponse("Hello, wanna sign up?  It's " + str(datetime.now()))
 
     template = loader.get_template("home/index.html")
-    return HttpResponse(template.render())
+    return HttpResponse(template.render(request=request)) # need to pass request to ge prompt message
 
 def register_page(request):
+    return render(request, "home/register_page.html")
     # return HttpResponse("Hello, wanna sign up?  It's " + str(datetime.now()))
 
-    latest_users = UserAccount.objects.order_by("-reg_time")[:5]
+    latest_users = User.objects.order_by("-date_joined")[:5]
     # output = ", ".join([u.username for u in latest_users])
     # return HttpResponse(output)
 
@@ -35,18 +38,19 @@ def register_authn(request):
     new_username = request.POST["username"]
     new_password = request.POST["password"]
     print(new_username)
-    u = UserAccount.objects.filter(username=new_username)
+    u = User.objects.filter(username=new_username)
     print(u)
     
     if u.exists():
-        target_url = 'https://geeksforgeeks.org'
-        # return render(request, 'home/index.html', {'target_url': target_url})
         print("this username exists")
         messages.success(request, "This username already exists, please choose another one")
         return redirect("register_page")
     else:
-        new_user_data = UserAccount(username=new_username, password=new_password)
-        new_user_data.save()
+        # new_user = User(username=new_username, password=new_password) # this will not encrypt password
+        new_user = User.objects.create_user(username=new_username, password=new_password)
+        new_user.save()
+
+        messages.success(request, "Account registered!")
         return redirect("index")
 
 
@@ -68,26 +72,17 @@ def login_authn(request):
     input_password = request.POST["password"]
     print(input_username)
     
-    user_data = UserAccount.objects.filter(username=input_username)
-    print(user_data)
+    user = authenticate(request, username=input_username, password=input_password)
     
-    if user_data.exists():
-        target_url = 'https://geeksforgeeks.org'
-        # return render(request, 'home/index.html', {'target_url': target_url})
-
-        print("great, this username exists")
-        
-        if input_password == user_data[0].password:
-            print("password matches")
-            request.session["cur_user_pk"] = user_data[0].pk
-            return redirect("password_main")
-        else:
-            print("wrong password")
-            messages.success(request, "Wrong password")
-            return redirect("login_page")
+    if user is not None:
+        print("password matches")
+        login(request, user)
+        # request.session["cur_user_pk"] = user.pk
+        messages.success(request, "login succeeds")
+        return redirect("password_main")
     else:
-        print("This username doesn't exists")
-        messages.success(request, "This username doesn't exists")
+        print("Wrong user name or password")
+        messages.success(request, "Wrong user name or password")
         return redirect("login_page")
     
 
@@ -102,8 +97,9 @@ def password_add_new(request):
     input_password = request.POST["password"]
     print(", ".join((input_subject, input_account, input_password)))
 
-    cur_username = request.session.get("cur_username")
-    query = PasswordData.objects.filter(subject=input_subject, account=input_account, user=cur_username)
+    cur_user = request.user
+    query = cur_user.passworddata.filter(subject=input_subject, account=input_account)
+    print(len(query))
 
     if query.exists():
         print("This data already exists")
@@ -112,24 +108,26 @@ def password_add_new(request):
     else:
         print("let's add new data")
         cur_user_pk = request.session.get("cur_user_pk")
-        cur_user = UserAccount.objects.get(pk=cur_user_pk)
         cur_user.passworddata.create(subject=input_subject, account=input_account, password=input_password)
+
         messages.success(request, "New data added")
         return redirect("password_main")
 
-    return render(request, "home/password_main.html")
-
 
 def password_lookup(request):
-    input_subject = request.POST["subject"]
-    print(input_subject)
+    cur_user = request.user
 
-    cur_username = request.session.get("cur_username")
-    query = PasswordData.objects.raw("select * from home_passworddata where user_id = %s and instr(subject, %s) > 0", [cur_username, input_subject])
-    PasswordData.objects.raw("DROP TABLE signup_useraccount;")
-    print("let's look it up")
-    
-    print(len(query))
+    if "subject" in request.POST:
+        print("yes")
+        input_subject = request.POST["subject"]
+        print(input_subject)
+        query = PasswordData.objects.raw("select * from home_passworddata where user_id = %s and instr(subject, %s) > 0", [cur_user.id, input_subject])
+    else:
+        print("no")
+        query = cur_user.passworddata.all()
+
+
+    print("length: ", len(query))
     
     context = {
         "password_data_lookup": query,
@@ -137,4 +135,6 @@ def password_lookup(request):
     
     return render(request, "home/password_lookup.html", context)
 
+def password_list_all(request):
+    cur_user = request.user
 
